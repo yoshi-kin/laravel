323603a2fca38f2fcddf3577b7414a8687ea81a7
yoshikifujiwara <kinrou2016@gmail.com>
2020-12-05 23:20:10 +0900

    first commit

---
+++ app/Http/Controllers/TopPageController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+
+class TopPageController extends Controller {
+
+    public function show(){
+       return view('top');
+    }
+}
---
+++ resources/views/top.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel='stylesheet' href="{{ asset('css/style.css') }}">
+</head>
+<body>
+@include('parts.header')
+<main>
+	<h1>これはトップページです</h1>
+</main>
+@include('parts.footer')
+</body>
+</html>
